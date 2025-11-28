caa8fd67e97c9c8824fcad32f000d22b6984ebd3
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-11-28 11:03:47 +0100

    Created a helper function to add the color codes

---
+++ nirf-tlr-assistant/helpers/ansi_color_codes.py
@@ -0,0 +1,7 @@
+# ANSI color codes for terminal logging (won't affect Tkinter Text directly)
+COLOR_HEADER = "\033[95m"
+COLOR_OK = "\033[92m"
+COLOR_INFO = "\033[94m"
+COLOR_WARNING = "\033[93m"
+COLOR_ERROR = "\033[91m"
+COLOR_RESET = "\033[0m"
